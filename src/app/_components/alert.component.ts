4176216a018eb97f2fdf53502bc242e9eebc607a
unknown <MM520@ccs.local>
2025-04-04 12:35:28 +0800

    initial commit

---
+++ src/app/_components/alert.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+import { Alert, AlertType } from '@app/_models';
+import { AlertService } from '@app/_services';
+
+@Component({ selector: 'alert', templateUrl: 'alert.component.html' })
+export class AlertComponent implements OnInit, OnDestroy {
+    @Input() id = 'default-alert';
+    @Input() fade = true;
+
+    alerts: Alert[] = [];
+    alertSubscription: Subscription;
+    routeSubscription: Subscription;
+
+    constructor(private router: Router, private alertService: AlertService) {
+        ngOnInit() {
+            // Subscribe to new alert notifications
+            this.alertSubscription = this.alertService.onAlert(this.id)
+                .Subscribe(alert => {
+                    //
+                })
+        }
+    }
+}
